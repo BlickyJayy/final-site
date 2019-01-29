96dd93260c6b284787d0dad3790dc6705e63b318
BlickyJayy <BlickyJayy@users.noreply.github.com>
2019-01-28 23:39:27 -0800

    test

--- projects.js
+++ projects.js
@@ -3,34 +3,6 @@ $(document).ready(function() {
   var slideshowSpeed = 3000
   var slideshowIntervalID = setInterval(slideshow, slideshowSpeed)
 
-  $('.slideshow-image').click(function() {
-    clearInterval(slideshowIntervalID)
-    $('.slideshow-overlay').fadeIn(200)
-    $(this).addClass('slideshow-viewer')
-  })
-
-  $('.slideshow-overlay').click(function() {
-    var clicks = $(this).data('clicks');
-    if (clicks) {
-    $('.slideshow-viewer').removeClass('slideshow-viewer')
-    $('.slideshow-overlay').fadeOut(200)
-    setInterval(slideshow, slideshowSpeed)
-  }
-   else (clicks) {
-    $('.slideshow-viewer').addClass('slideshow-viewer')
-    $('.slideshow-overlay').fadeIn(200)
-    setInterval(slideshow, slideshowSpeed)
-  }
-
-  })
-
-  function slideshow() {
-    $('.slideshow-image').fadeOut(400).eq(slideNumber).delay(400).fadeIn(400);
-    slideNumber = slideNumber + 1
-    if ( slideNumber  == $('.slideshow-image').length) {
-      slideNumber = 0;
-    }
-  }
 
 
   $('.project-filter a').click(function() {
